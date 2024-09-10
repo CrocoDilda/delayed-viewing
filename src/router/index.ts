3618f07fc8ee56afc153522b7f5e982df9b1cc6c
CrocoDilda <cucuprum4k@gmail.com>
2024-09-10 18:59:11 +0300

    configured vue router, moved styles from public folder to assets folder, corrected error in component name sing up

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "welcome",
+      component: () => import("../views/WelcomePage/WelcomePage.vue"), // это ленивая загрузка. Компонент будет импортирован при необзодимости, поэтому не надо импортировать его сверху
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      path: "/login",
+      name: "login",
+      component: () => import("../views/WelcomePage/LoginForm/LoginForm.vue"),
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: () => import("../views/WelcomePage/SignUpForm/SignUpForm.vue"),
+    },
+  ],
 })
 
 export default router
---
+++ src/views/WelcomePage/SignUpForm/SignUpForm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+function goToLogin() {
+  router.push("/login")
+}
+</script>
+
+<template>
+  <h2>sing up</h2>
+  <button @click="goToLogin">To login</button>
+</template>
